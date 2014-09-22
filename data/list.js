799c18ac484c152dd1553281267ad6fc8f11916d
Hwan Oh <hwangoon@gmail.com>
2014-09-23 02:51:47 +0900

    lol_tier 추가

--- data/list.js
+++ data/list.js
@@ -4,5 +4,6 @@ var List = [
 	["adscore", "선악지수 측정기"],
 	["alchlchk", "주량 측정기"],
 	["alchlchk_type", "내 몸에 맞는 술 알아보기"],
-	["blood_keyword", "내 혈액형에 어울리는 키워드"]
+	["blood_keyword", "내 혈액형에 어울리는 키워드"],
+	["lol_tier", "다음시즌 나의 롤 티어는?"]
 ];
