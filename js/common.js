ea0478c0c98680727e92daf81aef5c960047356c
Hwan Oh <hwangoon@gmail.com>
2014-10-20 17:59:15 +0900

    fb test

--- js/common.js
+++ js/common.js
@@ -118,10 +118,10 @@ var getResult = function(type) {
 			"app_id=457408721067962&"+
 			"link="+encodeURIComponent(App.resultUrl)+"&"+
 			"picture="+encodeURIComponent(App.thumbnail)+"&"+
-			"name="+encodeURIComponent('카카오테스트 - ' + App.title)+"&"+
-			"caption="+encodeURIComponent('['+App.title+'] 실행결과')+"&"+
-			"description="+encodeURIComponent(resultText)+"&"+
-			"message="+encodeURIComponent(resultText)+"&"+
+			"name="+'카카오테스트 - ' + App.title+"&"+
+			"caption="+'['+App.title+'] 실행결과'+"&"+
+			"description="+resultText+"&"+
+			"message="+resultText+"&"+
 			"redirect_uri="+encodeURIComponent(App.resultUrl);
 			/*
 				http://www.facebook.com/dialog/feed?
